c7f8410856399618d1679bbb8c4d0ec4e925bad5
Yurback <ryv1983@mail.ru>
2021-03-14 18:50:25 +0300

    pagination

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const multer = require('multer');
+const {v4: uuidv4} = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
+const MONGODB_URI =
+  'mongodb+srv://yuriy:ccLs1u3VFU7aBFK2@cluster0.gyckc.mongodb.net/messages';
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) =>{
+        cb(null, uuidv4() + "_" + file.originalname ) // new Date().toISOString() + '-' + = для Мфс и для Linux
+    } // или cb(null, Date.now() + "_" + file.originalname )
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(
+    multer({storage: fileStorage, fileFilter: fileFilter}).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images' )));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,4 +45,15 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next)=>{
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+})
+
+mongoose.connect(MONGODB_URI)
+.then(result=>{
+    app.listen(8080);
+})
+.catch(err=>console.log(err))
